870edf1cb33eba9dbbfb902ae060c849ba62283b
Donia Ahmed <112786733+DoniaAhmed20@users.noreply.github.com>
2024-03-04 11:57:26 +0200

    Update user_middleware.py

--- users/user_middleware.py
+++ users/user_middleware.py
@@ -13,3 +13,20 @@
 
 #         response = self.get_response(request)
 #         return response
+
+
+from django.shortcuts import redirect
+from django.contrib import messages
+from django.urls import reverse
+
+class LoginRequiredMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        if not request.user.is_authenticated and request.path == reverse('my_profile'):
+            messages.error(request, 'You must be logged in to view this page.')
+            return redirect('login')  # Redirect to the login page
+        return response
+
